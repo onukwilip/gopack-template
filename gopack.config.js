4c146ac4211ec27ca0182d51f99473dd91da4a8f
onukwilip <onukwilip@gmail.com>
2023-03-24 11:50:03 +0100

    Added the gopack.config.js to template folder, also added program to install gopack.config.js during `gopack init`

---
+++ gopack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  // generateCSSFiles: false,
+  // devtool: false,
+  // useCoreJs: false,
+  // entry: {
+  //   index: "./src/index.js",
+  // },
+  // outputFilenameFormat: "[name].chunk.js",
+  // outputFilename: "main.js",
+  // pages: [
+  //   {
+  //     template: path.resolve(__dirname, "src/index.html"),
+  //     chunk: ["index"],
+  //     filename: "index.html",
+  //   }
+  // ],
+  // assetsFolder: "assets",
+};
